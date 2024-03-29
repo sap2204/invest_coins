8ff1b7f0a9832d6d49da0ee0d6e7abb99c5506ea
sap2204 <tbksap@gmail.com>
2024-03-29 15:45:39 +0300

    Correct class for simple sites. Testid with dalena bank

--- 1_dalena.py
+++ 1_dalena.py
@@ -1,11 +1,27 @@
 from base_simple_scraper import SimpleScraper
+from requests_html import AsyncHTMLSession
+import asyncio
 
 dalena = SimpleScraper("https://www.dalenabank.ru/chastnym-klientam/monety/")
 
-dalena.get_data_from_bank()
+async def main():
+    asession = AsyncHTMLSession()
+    task = dalena.get_data_from_bank(asession,
+                                     coin_list_selector = '.bx-newslist-block',
+                                     coin_name_selector = '.bx-newslist-title',
+                                     name_index = 0,
+                                     coin_weight_selector = '.bx-newslist-other',
+                                     weight_index = 0,
+                                     coin_me_selector = '.bx-newslist-other',
+                                     me_index = 1,
+                                     coin_nominal_selector = '.bx-newslist-other',
+                                     nominal_index = 2,
+                                     coin_price_selector = '.bx-newslist-other',
+                                     price_index = 3)
+    return await asyncio.gather(task)
 
-
-
+results = asyncio.run(main())
+print(results)
 '''
 from requests_html import AsyncHTMLSession
 from fake_useragent import UserAgent
--- base_simple_scraper.py
+++ base_simple_scraper.py
@@ -5,6 +5,9 @@ import requests
 
 
 class SimpleScraper:
+    '''
+    Класс для парсинга обычных сайтов, код которых генерируется на сервере
+    '''
     def __init__(self, url:str):
         # Паттерны регулярных выражений
         self.weight_pattern = r'\d+[.,]\d+'
@@ -16,15 +19,13 @@ class SimpleScraper:
         self.user = UserAgent().random
         self.header = {"user_agent": self.user}
 
-        # Асинхронная сессия
-        self.asession = AsyncHTMLSession()
-
+        
         # Адрес сайта
         self.url = url
 
 
         # Метод получения данных с сайтов
-        async def get_data_from_bank(self,
+    async def get_data_from_bank(self, asession,
                                      coin_list_selector = None,
                                      coin_name_selector = None,
                                      name_index = 0,
@@ -33,28 +34,42 @@ class SimpleScraper:
                                      coin_me_selector = None,
                                      me_index = 0,
                                      coin_nominal_selector = None,
-                                     nominal_index = 0
+                                     nominal_index = 0,
                                      coin_price_selector = None,
                                      price_index = 0
                                      ):
+            
             try:
-                r = await self.asession.get(url=self.url, headers=self.header)
+                r = await asession.get(url=self.url, headers=self.header)
+
+                # Список монет
+                data = []
+
                 coin_list = r.html.find(coin_list_selector)
 
                 for coin in coin_list:
-                    coin_name = coin.find(coin_name_selector)[name_index].text
+                    # Получения данных со страницы
 
-                    coin_weight = coin.find(coin_weight_selector)[weight_index].text
+                    coin_name = coin.find(coin_name_selector)[name_index].text.strip()
 
-                    coin_me = coin.find(coin_me_selector)[me_index].text
+                    coin_we = coin.find(coin_weight_selector)[weight_index].text.strip().replace(' ', '')
+                    coin_weight = re.search(self.weight_pattern, coin_we).group(0)
 
-                    coin_nominal = coin.find(coin_nominal_selector)[nominal_index].text
+                    coin_met = coin.find(coin_me_selector)[me_index].text.strip().lower()
+                    coin_me = re.search(self.me_pattern, coin_met).group(0)  + ' ' + re.search(r'\d+', coin_met).group(0) + '-'
 
-                    coin_price = coin.find(coin_price_selector)[price_index].text
+                    coin_nom = coin.find(coin_nominal_selector)[nominal_index].text.strip()
+                    coin_nominal = re.search(self.nominal_pattern, coin_nom).group(0) + '-' , # добавление разделителя '-'
 
+                    coin_pr = coin.find(coin_price_selector)[price_index].text.strip().replace(' ', '')
+                    coin_price = int(re.search(self.price_pattern, coin_pr).group(0))
+                
+                                      
+
+                    data.append([coin_name, '', coin_price, coin_weight, coin_me, coin_nominal])
             except requests.exceptions.RequestException as e:
                 print(e)
             
-            return coin_name, coin_weight, coin_me, coin_nominal, coin_price
+            return data
 
             
